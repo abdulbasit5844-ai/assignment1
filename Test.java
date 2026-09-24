7dfe12138106b9020baa186f4b1058bef2ab8cf1
Abdul Basit <ab3296726@gmail.com>
2026-09-24 21:05:54 +0500

    senior

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello i am a student!!");
+        System.out.println("hello i am a senior!!");
     }
 }
